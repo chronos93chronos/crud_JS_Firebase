96ceff5516a2b1d6901d66e93d864e655375d070
ALEXANDER HERNANDEZ <111362778+chronos93chronos@users.noreply.github.com>
2023-05-09 17:34:35 -0300

    Add files via upload
    creacion de archivos base para trabajar, y conexion a BD Firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB-Vv8D0ByLExs4IsBQ1JdOMHAYje37joI",
+    authDomain: "crud-carrito-js-95126.firebaseapp.com",
+    projectId: "crud-carrito-js-95126",
+    storageBucket: "crud-carrito-js-95126.appspot.com",
+    messagingSenderId: "81726940821",
+    appId: "1:81726940821:web:984a4389d0b3096d989d9c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
